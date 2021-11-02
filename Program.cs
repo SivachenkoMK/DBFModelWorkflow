d091f82a6f65b487a5cd8f0a3786cedc31b90b4f
Mikhail Syvachenko <misha.sivachenko@gmail.com>
2021-11-02 14:12:06 +0200

    Added all files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidzyTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VidzyEntities entities = new VidzyEntities();
+            entities.AddVideo("Lol, video", DateTime.Now, "Horror", 1);
+        }
+    }
+}
